5346190312b9ae6c82aebefb4c56dc175d029714
munna-68 <mahamudmunna68@gmail.com>
2025-01-15 21:54:03 +0600

    adding most of the functionality

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const container = document.querySelector('.container');
-const div = document.createElement('div');
 
-div.classList.add('div');
-container.appendChild(div);
+for(let i = 0; i <= 256; i++) {
+  const div = document.createElement('div');
+  div.classList.add('div');
+  container.appendChild(div);
+  div.addEventListener('click', function() {
+    div.style.backgroundColor = 'black';
+  });
+}
+
